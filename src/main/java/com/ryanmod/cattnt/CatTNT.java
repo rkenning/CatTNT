02307a2f6474ab8ec5be59616cf84fe29d2da6f3
Ryan <rkenning2@yahoo.co.uk>
2014-06-25 19:00:54 +0100

    Basic Block Created

---
+++ src/main/java/com/ryanmod/cattnt/CatTNT.java
@@ -0,0 +1,12 @@
+package com.ryanmod.cattnt;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class CatTNT extends Block {
+
+	public CatTNT() {
+		super(Material.tnt);
+	}
+
+}
