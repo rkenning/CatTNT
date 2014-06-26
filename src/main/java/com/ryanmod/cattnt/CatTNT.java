bdaeed503ad7a493c192e145ae2e224237d97652
Ryan Kenning <contact@rkitsolutions.co.uk>
2014-06-26 09:07:12 +0100

    Some further Changes

--- src/main/java/com/ryanmod/cattnt/CatTNT.java
+++ src/main/java/com/ryanmod/cattnt/CatTNT.java
@@ -1,12 +1,40 @@
-package com.ryanmod.cattnt;
+package net.ryanmod.cattnt;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
 
-public class CatTNT extends Block {
+@Mod(modid = CatTNT.modid, version = CatTNT.version)
+public class CatTNT {
 
-	public CatTNT() {
-		super(Material.tnt);
+	public static final String modid = "CatTNT";
+	public static final String version = "1.1";
+	
+	public static Block blockRed;
+	
+	public static CreativeTabs ryanTab = new CreativeTabs("RyanMods"){
+		public Item getTabIconItem() {
+			return Items.emerald;
+		}		
+	};
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent e){
+		blockRed = new BlockRed().setBlockName("blockRed").setCreativeTab(ryanTab);
+		GameRegistry.registerBlock(blockRed, "blockRed");
 	}
-
+	
+	
+	@EventHandler
+	public void init(FMLInitializationEvent e){
+		
+	}
+	
 }
+
