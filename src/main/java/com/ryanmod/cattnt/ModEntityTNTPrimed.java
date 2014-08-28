a51aaa07b6ffdfda35d7653d812520ac4865020a
Ryan Kenning <contact@rkitsolutions.co.uk>
2014-08-28 10:31:21 +0100

    Revert "Broken quite a few things but needed to tidy up and extend base classes"
    This reverts commit 41745f78878bd55564d24a4b18f002e90630b17a.

--- src/main/java/com/ryanmod/cattnt/ModEntityTNTPrimed.java
+++ src/main/java/com/ryanmod/cattnt/ModEntityTNTPrimed.java
@@ -10,13 +10,10 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.entity.ai.EntityAISit;
-import net.minecraft.entity.item.EntityTNTPrimed;
 import net.minecraft.entity.passive.EntityOcelot;
 import net.minecraft.entity.passive.EntityTameable;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.SharedMonsterAttributes;
-
-
 
 
 
@@ -30,18 +27,21 @@ import java.util.Iterator;
 import java.util.List;
 
 
-	public class ModEntityTNTPrimed extends EntityTNTPrimed {
+	public class ModEntityTNTPrimed extends Entity {
 
-		
-	   
-		public ModEntityTNTPrimed(World par1World) {
+	    /** How long the fuse is */
+	    public int fuse;
+	    private EntityLivingBase tntPlacedBy;
+	    private static final String __OBFID = "CL_00001681";
+
+	    public ModEntityTNTPrimed(World par1World)
+	    {
 	        super(par1World);
 	        this.preventEntitySpawning = true;
 	        this.setSize(0.98F, 0.98F);
 	        this.yOffset = this.height / 2.0F;
-		}
-		
-	   
+	    }
+
 	    public ModEntityTNTPrimed(World par1World, double par2, double par4, double par6, EntityLivingBase par8EntityLivingBase)
 	    {
 	        this(par1World);
@@ -56,13 +56,67 @@ import java.util.List;
 	        this.prevPosZ = par6;
 	        //this.tntPlacedBy = par8EntityLivingBase;
 	    }
-		
-		
-		private void explode()
+
+	    protected void entityInit() {}
+
+	    /**
+	     * returns if this entity triggers Block.onEntityWalking on the blocks they walk on. used for spiders and wolves to
+	     * prevent them from trampling crops
+	     */
+	    protected boolean canTriggerWalking()
+	    {
+	        return false;
+	    }
+
+	    /**
+	     * Returns true if other Entities should be prevented from moving through this Entity.
+	     */
+	    public boolean canBeCollidedWith()
+	    {
+	        return !this.isDead;
+	    }
+
+	    /**
+	     * Called to update the entity's position/logic.
+	     */
+	    public void onUpdate()
+	    {
+	        this.prevPosX = this.posX;
+	        this.prevPosY = this.posY;
+	        this.prevPosZ = this.posZ;
+	        this.motionY -= 0.03999999910593033D;
+	        this.moveEntity(this.motionX, this.motionY, this.motionZ);
+	        this.motionX *= 0.9800000190734863D;
+	        this.motionY *= 0.9800000190734863D;
+	        this.motionZ *= 0.9800000190734863D;
+
+	        if (this.onGround)
+	        {
+	            this.motionX *= 0.699999988079071D;
+	            this.motionZ *= 0.699999988079071D;
+	            this.motionY *= -0.5D;
+	        }
+
+	        if (this.fuse-- <= 0)
+	        {
+	            this.setDead();
+
+	            if (!this.worldObj.isRemote)
+	            {
+	                this.explode();
+	            }
+	        }
+	        else
+	        {
+	            this.worldObj.spawnParticle("smoke", this.posX, this.posY + 0.5D, this.posZ, 0.0D, 0.0D, 0.0D);
+	        }
+	    }
+
+	    private void explode()
 	    {
 	        //float f = 224.0F;
-	        float f = 8F;
-	        //this.worldObj.createExplosion(this, this.posX, this.posY, this.posZ, f, true);
+	        float f = 0F;
+	        this.worldObj.createExplosion(this, this.posX, this.posY, this.posZ, f, true);
 	        
 	        // Playing with Fireworks -- Not quite working yet!
 	        //NBTTagCompound NBTTagCompound = null;
@@ -128,6 +182,34 @@ import java.util.List;
 			
 	    }
 
-	 
+	    /**
+	     * (abstract) Protected helper method to write subclass entity data to NBT.
+	     */
+	    protected void writeEntityToNBT(NBTTagCompound par1NBTTagCompound)
+	    {
+	        par1NBTTagCompound.setByte("Fuse", (byte)this.fuse);
+	    }
+
+	    /**
+	     * (abstract) Protected helper method to read subclass entity data from NBT.
+	     */
+	    protected void readEntityFromNBT(NBTTagCompound par1NBTTagCompound)
+	    {
+	        this.fuse = par1NBTTagCompound.getByte("Fuse");
+	    }
+
+	    @SideOnly(Side.CLIENT)
+	    public float getShadowSize()
+	    {
+	        return 0.0F;
+	    }
+
+	    /**
+	     * returns null or the entityliving it was placed or ignited by
+	     */
+	    public EntityLivingBase getTntPlacedBy()
+	    {
+	        return this.tntPlacedBy;
+	    }
 	}
 
