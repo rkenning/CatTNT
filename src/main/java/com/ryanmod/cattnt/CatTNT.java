3973e2b799c9d09f035a2d1ed8123fc1be37bc65
Ryan Kenning <contact@rkitsolutions.co.uk>
2014-09-08 18:11:10 +0100

    Slightly broken due to new render class but last commit for today

--- src/main/java/com/ryanmod/cattnt/CatTNT.java
+++ src/main/java/com/ryanmod/cattnt/CatTNT.java
@@ -9,6 +9,9 @@ import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.client.registry.RenderingRegistry;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 
 @Mod(modid = CatTNT.modid, version = CatTNT.version)
 public class CatTNT {
@@ -24,17 +27,28 @@ public class CatTNT {
 		}		
 	};
 	
-	@EventHandler
+	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent e){
 		blockCatTNT = new BlockCatTNT().setBlockName("CatTNT").setCreativeTab(ryanTab).setBlockTextureName(modid+ ":" + "cat_tnt_side");
 		
 		GameRegistry.registerBlock(blockCatTNT, "CatTNT");
 	}
 	
-	
-	@EventHandler
-	public void init(FMLInitializationEvent e){
-		
+	  @Mod.EventHandler
+	  public void init(FMLInitializationEvent event) {
+	    loadRecipes();
+	    loadRenderers();
+	  }
+
+	  public void loadRecipes() {
+	    GameRegistry.addRecipe(new ItemStack(blockCatTNT, 1), new Object[] { "X#X", "#X#", "X#X", Character.valueOf('X'), Items.gunpowder, Character.valueOf('#'), Blocks.red_flower });
+  }
+
+	  public void loadRenderers() {
+	    EntityRegistry.registerModEntity(EntityPlantTNTPrimed.class, "plantTntPrimed", 1, this, 80, 3, true);
+	    if (FMLCommonHandler.instance().getSide().isClient())
+	      RenderingRegistry.registerEntityRenderingHandler(EntityPlantTNTPrimed.class, new RenderPlantTNTPrimed());
+	  }
 	}
 	
 }
